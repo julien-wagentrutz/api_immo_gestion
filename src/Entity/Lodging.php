3cf0e92c737a3ba196e76881cd0c64430100a628
julien Wagentrutz <julien.wagentrutz@hetic.net>
2020-11-06 16:30:26 +0100

    :sparkles: Add Lodging entity

---
+++ src/Entity/Lodging.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LodgingRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+
+
+/**
+ * @ORM\Entity(repositoryClass=LodgingRepository::class)
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Lodging
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="string", length=36, unique=true)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $state;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private $zipCode;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $complementAddress;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $lastUpdateAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zipCode;
+    }
+
+    public function setZipCode(?string $zipCode): self
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getComplementAddress(): ?string
+    {
+        return $this->complementAddress;
+    }
+
+    public function setComplementAddress(?string $complementAddress): self
+    {
+        $this->complementAddress = $complementAddress;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeInterface
+    {
+        return $this->createAt;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreateAt(): self
+    {
+        $this->createAt = new \DateTime();
+
+        return $this;
+    }
+
+    public function getLastUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->lastUpdateAt;
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function setLastUpdate(): self
+    {
+        $this->createAt = new \DateTime();
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setUpLastUpdate(): self
+    {
+        $this->lastUpdateAt = new \DateTime();
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setIdValue()
+    {
+        $uuid = Uuid::v4();;
+        $this->id = $uuid->jsonSerialize();
+    }
+
+}
